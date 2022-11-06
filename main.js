c1ba9e7619d55658d4c6ead5d625534868bb3894
Sergey Lipnevich <sergey@optimaltec.com>
2022-11-06 01:34:04 -0400

    working and displaying an action button

---
+++ main.js
@@ -0,0 +1,22 @@
+async function notify(title, message) {
+    await browser.notifications.create({
+        type: "basic",
+        title: title,
+        message: message
+    });
+}
+
+async function importCsv() {
+    const tabs = await browser.tabs.query({active: true, currentWindow: true});
+    console.log({tabs});
+    navigator.clipboard.writeText("hello").then(
+        async function() {
+            await notify("Copied", "Clipboard contains your info");
+        },
+        async function() {
+            await notfy("Failure", "Failed to copy to clipboard");
+        }
+    );
+}
+
+browser.browserAction.onClicked.addListener(importCsv);
